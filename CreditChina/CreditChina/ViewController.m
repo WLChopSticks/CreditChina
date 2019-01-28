965b41ac39354586911f157190e7794f93ffc48d
wanglei <wanglei_sy56@163.com>
2019-01-28 19:17:23 +0800

    init project

---
+++ CreditChina/CreditChina/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CreditChina
+//
+//  Created by 王磊 on 2019/1/28.
+//  Copyright © 2019 wanglei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
